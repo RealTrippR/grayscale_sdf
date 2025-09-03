c2dcdde3123ab4c4ed6cb013354e0d9cdf2d1b3a
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-09-03 12:09:58 -0400

    added description to sdf_imageToSdf

--- grayscale_sdf/grayscale_sdf.h
+++ grayscale_sdf/grayscale_sdf.h
@@ -90,8 +90,17 @@ typedef struct {
 } sdf_instance;
 
 
-
-/* returns 0 upon success, non-zero upon failure */
+/*
+@param sdf_instance* instance - sdf instance.
+@param sdf_imageInfo imageInfo - image information.
+@param uint16_t maxScanDist - the maximum distance in pixels of the field. Each value in the distance field will be a fraction of distance over this value.
+@param sdf_threshold* thresholds - thresholds to determine what is a feature and what is not a feature.
+@param uint16_t thresholdCount - number of thresholds.
+@param uint32_t distanceFieldSizeOut - the size (in bytes) of the distance field that will be generated.
+@param sdf_format distanceFieldFormat - the requested output format for the distance field. Only 4 valid options exist: SDF_FORMAT_R8, SDF_FORMAT_R16, SDF_FORMAT_R8G8, SDF_FORMAT_R16G16. If the format given has more than 1 channel, it will store direction (from 0-1, where 0 is right) in the second channel.
+@param int8_t* distanceFieldOut - the distance field buffer to write to.
+@return errno_t - returns 0 upon success, non-zero in the event of a failure.
+returns 0 upon success, non-zero upon failure */
 SDF_API errno_t sdf_imageToSdf(const sdf_instance* instance, const sdf_imageInfo* imageInfo, uint16_t maxScanDist, const sdf_threshold* thresholds, uint16_t thresholdCount, uint32_t* distanceFieldSizeOut, sdf_format distanceFieldFormat, int8_t* distanceFieldOut);
 
 #endif
